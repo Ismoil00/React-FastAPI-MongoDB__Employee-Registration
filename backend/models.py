de6648a1c8bf65730d3a0d5e398bba6bf58d1a76
Ismoil <juraev.abubakr_98@mail.ru>
2023-11-20 15:16:12 +0500

    initiation

---
+++ backend/models.py
@@ -0,0 +1,19 @@
+
+
+form = {
+    "first_name": "",
+    "last_name": "",
+    "email": "",
+    "profession": "",
+    "salary": 0.0,
+    "age": 0,
+    "gender": "",
+    "married": False,
+    "address": {
+        "city": "",
+        "street": "",
+        "building": "",
+        "home": 0,
+    },
+    
+}

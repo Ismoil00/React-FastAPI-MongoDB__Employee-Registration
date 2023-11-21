37016d76ba1992046eb300b51c0a7582729bd245
Ismoil <juraev.abubakr_98@mail.ru>
2023-11-21 11:15:52 +0500

    new codes

--- backend/routers.py
+++ backend/routers.py
@@ -2,9 +2,29 @@ from fastapi import APIRouter
 from models import Employee
 from config import employees_collection
 
+
 router = APIRouter()
 
 
+# get all employees:
+@router.get("/get-employees")
+async def get_employees():
+    pass
+
+
+# creating a employee:
 @router.post("/create-employee")
 async def create_employee(info: Employee):
+    pass
+
+
+# update a employee:
+@router.put("/update-employee/{id}")
+async def update_employee(id: str, info: Employee):
+    pass
+
+
+# delete a employee:
+@router.delete("/delete-employee/{id}")
+async def delete_employee(id: str):
     pass
